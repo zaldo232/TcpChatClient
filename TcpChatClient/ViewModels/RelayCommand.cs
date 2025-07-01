b84a88207cc3d7ff3a970610464ec77693497d03
zaldo232 <zaldo232@naver.com>
2025-07-01 14:53:00 +0900

    뷰, 코드비하인드 주석 추가

--- TcpChatClient/ViewModels/RelayCommand.cs
+++ TcpChatClient/ViewModels/RelayCommand.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace TcpChatClient.ViewModels
 {
+    // 매개변수 없는 MVVM 커맨드 구현 (버튼 등 바인딩용)
     public class RelayCommand : ICommand
     {
-        private readonly Action _execute;
+        private readonly Action _execute;   // 실행 델리게이트
 
         public RelayCommand(Action execute)
         {
             _execute = execute;
         }
 
-        public event EventHandler? CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged; // (항상 실행 가능 -> 별도 사용X)
 
-        public bool CanExecute(object? parameter) => true;
+        public bool CanExecute(object? parameter) => true; // 항상 실행 가능
 
         public void Execute(object? parameter)
         {
@@ -22,23 +22,26 @@ namespace TcpChatClient.ViewModels
         }
     }
 
+    // 매개변수 있는 MVVM 커맨드 구현
     public class RelayCommand<T> : ICommand
     {
-        private readonly Action<T> _execute;
+        private readonly Action<T> _execute;    // 실행 델리게이트
 
         public RelayCommand(Action<T> execute)
         {
             _execute = execute;
         }
 
-        public event EventHandler? CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged;   // (항상 실행 가능 -> 별도 사용X)
 
-        public bool CanExecute(object? parameter) => true;
+        public bool CanExecute(object? parameter) => true;  // 항상 실행 가능
 
         public void Execute(object? parameter)
         {
             if (parameter is T value)
-                _execute(value);
+            { 
+                _execute(value); 
+            }
         }
     }
 
--- TcpChatClient/Views/MainWindow.xaml.cs
+++ TcpChatClient/Views/MainWindow.xaml.cs
@@ -1,66 +1,85 @@
 ﻿using System.Collections.Specialized;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
-using TcpChatClient.Models;
-using TcpChatClient.ViewModels;
-using System.IO;
 using TcpChatClient.Helpers;
+using TcpChatClient.ViewModels;
 
 namespace TcpChatClient.Views
 {
+    // 메인 채팅창(윈도우) 코드비하인드
     public partial class MainWindow : Window
     {
-        private MainViewModel _vm;
+        private MainViewModel _vm;  // 뷰모델 참조
 
+        // 생성자(유저명 지정)
         public MainWindow(string username)
         {
             InitializeComponent();
-            _vm = new MainViewModel(username);
-            DataContext = _vm;
+            _vm = new MainViewModel(username); // 뷰모델 생성
+            DataContext = _vm;                // 바인딩
 
+            // 메시지 추가 시 자동 스크롤/읽음처리
             _vm.FilteredMessages.CollectionChanged += (_, e) =>
             {
                 if (e.Action == NotifyCollectionChangedAction.Add)
-                    scrollViewer.ScrollToEnd();
+                { 
+                    scrollViewer.ScrollToEnd(); 
+                }
 
                 TryMarkAsRead();
             };
 
+            // 스크롤/윈도우 활성화 시 읽음처리
             scrollViewer.ScrollChanged += (s, e) => TryMarkAsRead();
             this.Activated += (_, _) => TryMarkAsRead();
         }
 
+        // 디자이너 미리보기용 생성자
         public MainWindow() : this("디자이너") { }
 
+        // 채팅창이 맨 아래일 때 읽음 처리
         private void TryMarkAsRead()
         {
             if (!IsActive || _vm == null || scrollViewer == null) return;
 
             bool atBottom = scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - 50;
-            if (atBottom)
-                _ = _vm.MarkMessagesAsReadIfVisible();
+            if (atBottom) 
+            { 
+                _ = _vm.MarkMessagesAsReadIfVisible(); 
+            }
+     
         }
 
+        // 전체 유저 보기 버튼 클릭
         private void ShowAllUsers_Click(object sender, RoutedEventArgs e)
         {
             if (DataContext is MainViewModel vm)
-                vm.SelectedFilter = "전체";
+            { 
+                vm.SelectedFilter = "전체"; 
+            }
         }
 
+        // 접속중 유저 보기 버튼 클릭
         private void ShowOnlineUsers_Click(object sender, RoutedEventArgs e)
         {
             if (DataContext is MainViewModel vm)
-                vm.SelectedFilter = "접속중";
+            { 
+                vm.SelectedFilter = "접속중"; 
+            }
         }
 
+        // 파일, 이미지 클릭(다운로드)
         private void FileDownload_Click(object sender, RoutedEventArgs e)
         {
             ChatMessage msg = null;
 
             if (sender is FrameworkElement fe && fe.Tag is ChatMessage m)
+            {
                 msg = m;
+            }
 
             if (msg == null || string.IsNullOrWhiteSpace(msg.Content)) return;
 
@@ -87,8 +106,7 @@ namespace TcpChatClient.Views
             }
         }
 
-
-
+        // 입력창 파일 드래그 허용
         private void InputBox_PreviewDragOver(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -98,6 +116,7 @@ namespace TcpChatClient.Views
             }
         }
 
+        // 입력창에 파일 드롭 -> 자동 전송
         private void InputBox_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -113,6 +132,7 @@ namespace TcpChatClient.Views
             }
         }
 
+        // 플레이스홀더(검색창) 포커스 진입시 텍스트 제거
         private void ClearPlaceholder(object sender, RoutedEventArgs e)
         {
             if (sender is TextBox tb && (tb.Text == "유저 검색" || tb.Text == "메시지 검색"))
@@ -122,31 +142,43 @@ namespace TcpChatClient.Views
             }
         }
 
+        // 검색창 포커스 아웃시 플레이스홀더 복구
         private void RestorePlaceholder(object sender, RoutedEventArgs e)
         {
             if (sender is TextBox tb && string.IsNullOrWhiteSpace(tb.Text))
             {
                 if (tb.Name == "UserSearchBox")
-                    tb.Text = "유저 검색";
+                { 
+                    tb.Text = "유저 검색"; 
+                }
                 else if (tb.Name == "MessageSearchBox")
+                {
                     tb.Text = "메시지 검색";
+                }
 
-                tb.Foreground = Brushes.Gray;
+                    tb.Foreground = Brushes.Gray;
             }
         }
 
+        // 유저 검색 텍스트 변경시 뷰모델 반영
         private void UserSearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (sender is TextBox tb && DataContext is MainViewModel vm && tb.Text != "유저 검색")
-                vm.UserSearchKeyword = tb.Text;
+            { 
+                vm.UserSearchKeyword = tb.Text; 
+            }
         }
 
+        // 메시지 검색 텍스트 변경시 뷰모델 반영
         private void MessageSearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (sender is TextBox tb && DataContext is MainViewModel vm && tb.Text != "메시지 검색")
-                vm.MessageSearchKeyword = tb.Text;
+            { 
+                vm.MessageSearchKeyword = tb.Text; 
+            }
         }
 
+        // 키워드 하이라이팅 표시 (메시지 텍스트 강조)
         public static void ApplyHighlightedText(TextBlock target, string fullText, string keyword)
         {
             if (string.IsNullOrWhiteSpace(keyword) || string.IsNullOrEmpty(fullText))
@@ -180,6 +212,7 @@ namespace TcpChatClient.Views
             }
         }
 
+        // 메시지 텍스트 블록 로드시 하이라이트 적용
         private void MessageTextBlock_Loaded(object sender, RoutedEventArgs e)
         {
             if (sender is TextBlock tb && tb.DataContext is ChatMessage msg && DataContext is MainViewModel vm)
@@ -189,6 +222,7 @@ namespace TcpChatClient.Views
             }
         }
 
+        // 윈도우 활성화시 현재 대화방 읽음 처리 강제
         protected override void OnActivated(EventArgs e)
         {
             base.OnActivated(e);
